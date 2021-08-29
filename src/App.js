9909f3db17f94827ba6c3e9df85beca995e4f063
Pedro Sandes <pedrosandes122@gmail.com>
2021-08-29 19:02:39 -0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Routes from "./routes";
+
+import "./styles/global.scss";
+import "./styles/reset.scss";
+import "./styles/fonts.scss";
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
