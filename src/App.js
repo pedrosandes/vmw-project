7ef0edc5ba3140a907f5d6598c4ac8804dc0eb71
Pedro Sandes <pedrosandes122@gmail.com>
2021-08-29 19:33:15 -0300

    feat: Implementando o estilo global a aplicação

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import Routes from "./routes";
-
-import "./styles/global.scss";
-import "./styles/reset.scss";
-import "./styles/fonts.scss";
+import React from 'react';
+import GlobalStyle from './styles/global';
+import Routes from './routes';
 
 export default function App() {
   return (
-    <Routes />
+    <>
+      <GlobalStyle />
+      <Routes />
+    </>
   );
 }
