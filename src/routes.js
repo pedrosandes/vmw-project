8283c7fcd88dc030d27c8c35d62ab0578f423562
Pedro Sandes <pedrosandes122@gmail.com>
2021-09-02 15:01:20 -0300

    feat: Implementando sistema de rotas

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import {
-  Home
-} from './pages';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Home } from "./pages";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/sobre" component={Home} />
+        <Route path="/agentes/:id" component={Home} />
+        <Route path="/modosdejogo" component={Home} />
+        <Route path="/mapa/:id" component={Home} />
+        <Route path="/ranques" component={Home} />
+        <Route path="*" component={Home} />
       </Switch>
     </BrowserRouter>
   );
