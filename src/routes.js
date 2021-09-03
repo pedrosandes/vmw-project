245d465006df3eb4a52506a6c12511ef624a9873
Pedro Sandes <pedrosandes122@gmail.com>
2021-09-03 14:25:30 -0300

    feat: Implementando Header nas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Home } from "./pages";
+import { Header } from "./components";
 
 export default function Routes() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/sobre" component={Home} />
