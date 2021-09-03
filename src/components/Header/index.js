fe25fcda3b3204c65d17130f1e5f16ecfdc50256
Pedro Sandes <pedrosandes122@gmail.com>
2021-09-03 14:31:31 -0300

    feat: Componente header criado

---
+++ src/components/Header/index.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import * as S from "./styles";
+import Logo from "../../assets/img/svg/logo.svg";
+import MenuOpenIcon from "../../assets/img/svg/menu-open-icon.svg";
+import MenuCloseIcon from "../../assets/img/svg/menu-close-icon.svg";
+import { Nav } from "./nav";
+import Routes from "./LinkRoutes";
+import { InputSearch } from "./inputSearch";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const ToggleMenu = () => {
+    return setIsOpen(!isOpen);
+  };
+
+  const CloseLinkOnClick = () => {
+    return setIsOpen(false);
+  };
+
+  window.addEventListener("resize", function () {
+    if (window.innerWidth > 897 && isOpen) {
+      setIsOpen(false);
+    }
+  });
+
+  return (
+    <>
+      <S.ContainerWrapper open={isOpen}>
+        <S.ContainerContent>
+          <Link to="/">
+            <S.Logo src={Logo} alt="Logo do Valorant Media Wiki" />
+          </Link>
+          <S.MenuIcon
+            onClick={ToggleMenu}
+            src={isOpen ? MenuCloseIcon : MenuOpenIcon}
+            alt="Icone do menu"
+          />
+          <S.ContainerLeft open={isOpen}>
+            <Nav />
+            <InputSearch />
+          </S.ContainerLeft>
+        </S.ContainerContent>
+      </S.ContainerWrapper>
+
+      <S.ContainerNav open={isOpen}>
+        <S.ListMobile open={isOpen}>
+          {Routes.map(({ id, name, path }) => (
+            <S.ListItemMobile key={id}>
+              <Link to={path} onClick={CloseLinkOnClick}>
+                {name}
+              </Link>
+            </S.ListItemMobile>
+          ))}
+        </S.ListMobile>
+      </S.ContainerNav>
+    </>
+  );
+};
+
+export default Header;
