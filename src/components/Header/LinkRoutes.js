83edfb303edb5f36c8b770edb2df26eb7e1b449e
Pedro Sandes <pedrosandes122@gmail.com>
2021-09-03 14:32:16 -0300

    feat: Implementando array com Links

---
+++ src/components/Header/LinkRoutes.js
@@ -0,0 +1,29 @@
+const routes = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "Agentes",
+    path: "/agentes",
+  },
+  {
+    id: 3,
+    name: "Mapas",
+    path: "/mapas",
+  },
+  {
+    id: 4,
+    name: "Modos de jogo",
+    path: "/modosdejogo",
+  },
+  {
+    id: 5,
+    name: "Ranques",
+    path: "/ranques",
+  },
+];
+
+export default routes;
