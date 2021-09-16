b891a5314a27d7eb1a0e62f3bd5142b9b4495d4f
Pedro Sandes <pedrosandes122@gmail.com>
2021-09-15 21:20:17 -0300

    chore: Importanto e exportando no index das pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import Ranques from "./Ranques";
 import Agentes from "./Agentes";
 import Mapas from "./Mapas";
 import Modos from "./Modos";
+import Sobre from "./Sobre";
 
-export { Agentes, Home, Mapas, Modos, Ranques };
+export { Agentes, Home, Mapas, Modos, Ranques, Sobre };
