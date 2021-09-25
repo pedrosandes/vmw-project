5d05e9302cba43435b6640693873647e10df2439
Ian Sandes <iansandes15@gmail.com>
2021-09-25 19:08:09 -0300

    feat: Implementando skeleton a seção Skeleton

--- src/components/Maps/index.js
+++ src/components/Maps/index.js
@@ -5,38 +5,48 @@ import * as S from "./styles";
 /* Components  */
 import SectionTitle from "../SectionTitle";
 import Button from "../Button";
+import MapsSkeleton from "./skeleton";
 
 /* Services  */
 import { getAllMaps } from "../../services/api";
 
 const Maps = () => {
   const [maps, setMaps] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     (async () => {
       const mapsResponse = await getAllMaps();
       const { data } = mapsResponse;
       setMaps(data);
+      setTimeout(() => {
+        setLoading(true);
+      }, 1000);
     })();
   }, []);
 
   return (
-    <S.MapsContainer>
-      <SectionTitle color="#333">Mapas</SectionTitle>
-      <S.MapsList>
-        {maps.slice(0, 6).map(({ id, name, image }) => {
-          return (
-            <S.MapsItem key={id}>
-              <S.MapImage src={image} title={name} />
-              <S.MapName>{name}</S.MapName>
-            </S.MapsItem>
-          );
-        })}
-      </S.MapsList>
-      <S.ButtonWrapper>
-        <Button to="/mapas">Ver todos os mapas</Button>
-      </S.ButtonWrapper>
-    </S.MapsContainer>
+    <>
+      {loading && (
+        <S.MapsContainer>
+          <SectionTitle color="#333">Mapas</SectionTitle>
+          <S.MapsList>
+            {maps.slice(0, 6).map(({ id, name, image }) => {
+              return (
+                <S.MapsItem key={id}>
+                  <S.MapImage src={image} title={name} />
+                  <S.MapName>{name}</S.MapName>
+                </S.MapsItem>
+              );
+            })}
+          </S.MapsList>
+          <S.ButtonWrapper>
+            <Button to="/mapas">Ver todos os mapas</Button>
+          </S.ButtonWrapper>
+        </S.MapsContainer>
+      )}
+      {!loading && <MapsSkeleton />}
+    </>
   );
 };
 
