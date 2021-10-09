f04b1bffd6a1cc6820f810a3e83e95cde5dc5fe0
Pedro Sandes <pedrosandes122@gmail.com>
2021-10-09 16:06:01 -0300

    feat: Implementando estilo do loading skeleton

--- src/components/GameModes/styles.js
+++ src/components/GameModes/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const Container = styled.section`
   margin-top: 100px;
@@ -42,5 +42,71 @@ export const Row = styled.div`
   @media (max-width: 561px) {
     justify-content: center;
     flex-direction: column;
+  }
+`;
+
+/* Skeleton  */
+
+const loading = keyframes`
+  0% {
+    transform: translateX(-150%);
+  }
+  50% {
+    transform: translateX(-60%);
+  }
+  100% {
+    transform: translateX(200%);
+  }
+`;
+
+export const ShimmerWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transform: skewX(-20deg);
+`;
+
+export const Shimmer = styled.div`
+  position: absolute;
+  width: 50%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.2);
+  animation: ${loading} 1s infinite;
+`;
+
+export const WrapperButton = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  overflow: hidden;
+`;
+
+export const Button = styled.div`
+  width: 300px;
+  min-height: 50px;
+  border-radius: 5px;
+  border: 2px solid ${({ theme }) => theme.colors.white};
+
+  @media screen and (max-width: 320px) {
+    width: 240px;
+  }
+`;
+
+export const GameModeCard = styled.div`
+  overflow: hidden;
+  width: 400px;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border: 3px solid ${({ theme }) => theme.colors.white};
+  border-radius: 5px;
+  box-shadow: 0px 0px 20px rgba(68, 68, 68, 0.3);
+
+  @media (max-width: 848px) {
+    width: 100%;
+    max-width: 400px;
+    min-width: 260px;
   }
 `;
