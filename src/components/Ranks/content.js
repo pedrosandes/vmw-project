fee1950c6a1b3a38f697c2d77f39d9f24c0e70ce
Pedro Sandes <pedrosandes122@gmail.com>
2021-10-09 17:11:54 -0300

    feat: Criando content mock dos ranques

---
+++ src/components/Ranks/content.js
@@ -0,0 +1,14 @@
+/* Assets  */
+import Platina from "../../assets/svg/platina.svg";
+import Diamante from "../../assets/svg/diamante.svg";
+import Imortal from "../../assets/svg/imortal.svg";
+import Radiante from "../../assets/svg/radiante.svg";
+
+const Ranks = [
+  { id: 1, name: "Platina", image: Platina },
+  { id: 2, name: "Diamante", image: Diamante },
+  { id: 3, name: "Imortal", image: Imortal },
+  { id: 4, name: "Radiante", image: Radiante },
+];
+
+export default Ranks;
