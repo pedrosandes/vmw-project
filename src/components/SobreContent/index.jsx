fe7a12f06f9226bc5dbf9bec89a1048825dc59b7
Pedro Sandes <pedrosandes122@gmail.com>
2021-11-14 22:30:49 -0300

    feat: Conteúdo de texto do saiba mais

--- src/components/SobreContent/index.jsx
+++ src/components/SobreContent/index.jsx
@@ -1,64 +1,124 @@
 import * as S from "./styles"
 
+import video from "../../assets/videos/video.mp4"
+import cape from "../../assets/img/capa-video.png"
+
+/* Content  */
+import { paragraphs, minimo, recomendados, premium } from "./content"
+
 const SobreContent = () => {
   return(
-    <S.Container>
-      <S.Navigation>
-        <p>O QUE É VALORANT?</p>
-        <p>Configurações do PC para jogar Valorant</p>
-        <p>Mínimo</p>
-        <p>Recomendados</p>
-        <p>Premium</p>
-        <p>Artigo completo</p>
-      </S.Navigation>
+    <S.Wrapper>
+      <S.Container>
+        <S.Navigation>
+          <S.Link href="#valorant">O QUE É VALORANT?</S.Link>
+          <S.Link href="#configs">Configurações do PC para jogar Valorant</S.Link>
+          
+          <ul>
+            <li>
+              <S.Link href="#minimos">Mínimo</S.Link>
+            </li>
+            <li>
+              <S.Link href="#recomendados">Recomendados</S.Link>
+            </li>
+            <li>
+              <S.Link href="#premium">Premium</S.Link>
+            </li>
+          </ul>
+          <S.Link href="#artigo">Artigo completo</S.Link>
+        </S.Navigation>
 
-      <S.ContentTexts>
+        <S.ContentTexts>
 
-        <S.Title>
-          Valorant: o que é e tudo que precisa saber sobre o FPS da Riot
-        </S.Title>
+          <S.Title>
+            Valorant: o que é e tudo que precisa saber sobre o FPS da Riot
+          </S.Title>
 
-        <S.Paragraph>
-          Jogo de tiro criado pela mesmo produtora de 
-          <span>League of Legends</span>, 
-          saiba tudo sobre Valorant e conheça os melhores 
-          produtos para aproveitar o FPS
-        </S.Paragraph>
+          <S.Paragraph>
+            Jogo de tiro criado pela mesmo produtora de{' '}
+            <span>League of Legends</span>, saiba tudo sobre 
+            Valorant e conheça os melhores produtos para aproveitar o FPS
+          </S.Paragraph>
 
-        <S.Paragraph>
-          Competindo com gigantes como CS:GO e Overwatch, 
-          Valorant é o jogo de tiro da Riot Games que já 
-          pode ser considerado um dos melhores jogos competitivos 
-          no Brasil, como mostramos em nossa lista.
-        </S.Paragraph>
+          <S.Paragraph>
+            {paragraphs.firstParagraph}
+          </S.Paragraph>
 
-        <S.Paragraph>
-        Até pouco tempo atrás, a Riot cuidava apenas da sua 
-        “galinha dos ovos de ouro”, conhecida como League of Legends. 
-        Só que a companhia quis expandir seu portfólio e abocanhar 
-        uma parte dos jogadores que não curtem tanto MOBA, ao mesmo 
-        tempo que atraindo a curiosidade de sua base de fãs.
-        </S.Paragraph>
+          <S.Paragraph>
+            {paragraphs.secondParagraph}
+          </S.Paragraph>
 
-        <S.Paragraph>
-          O resultado disso foi o colorido jogo de tiro com 
-          partidas 5x5, no qual cada jogador escolhe um “Agente" 
-          e deve conquistar objetivos para vencer a equipe inimiga. 
-          Tem curiosidade em conhecer o Valorant da Riot Games, mas 
-          ão quer perder tempo com um jogo que talvez não valha a pena?
-        </S.Paragraph>
-        
-        <S.Paragraph>
-         Então confira a seguir tudo o que você precisa saber 
-         sobre Valorant e conheça também alguns dos melhores 
-         produtos para aproveitar o game ao máximo! 
-        </S.Paragraph>
+          <S.Paragraph>
+            {paragraphs.thirdParagraph}
+          </S.Paragraph>
+          
+          <S.Paragraph>
+            {paragraphs.fourthParagraph}
+          </S.Paragraph>
 
-        <iframe src="https://www.youtube.com/watch?v=e_E9W2vsRbQ" width="766" height="400"/>
+          <S.Title id="valorant">O que é valorant?</S.Title>
 
-        
-      </S.ContentTexts>
-    </S.Container>
+          <S.Video controls poster={cape}>
+            <source src={video} type="video/mp4"/>
+          </S.Video>
+
+          <S.Paragraph>
+          Lançado em <span>2020</span>, Valorant é um jogo de tiro online 
+          e multiplayer no qual dois grupos com cinco jogadores 
+          se enfrentam nos diversos mapas de Valorant, inspirados 
+          em locais do mundo real. Dependendo do mapa disputado, 
+          cada lado da disputa deverá alcançar um objetivo determinado 
+          ou impedir o outro lado de conseguir conquistar essa meta.
+          </S.Paragraph>
+
+          <S.Paragraph>
+            {paragraphs.fifthParagraph}
+          </S.Paragraph>
+
+          <S.Paragraph>
+            {paragraphs.sixthParagraph}
+          </S.Paragraph>
+
+          <S.Paragraph>
+            {paragraphs.seventhParagraph}
+          </S.Paragraph>
+
+          <S.Title id="configs">Configurações do PC para jogar Valorant</S.Title>
+
+          <S.Paragraph>
+            {paragraphs.eighthParagraph}
+          </S.Paragraph>
+
+          <S.Paragraph>
+          {paragraphs.ninthParagraph}
+          </S.Paragraph>
+
+          <S.Title id="minimos">Requisitos mínimos</S.Title>
+
+          <S.List>
+            {minimo.map(item =>  <S.ListItem>{item}</S.ListItem>) }
+          </S.List>
+          
+          <S.Title id="recomendados">Requisitos Recomendados</S.Title>
+
+          <S.List>
+          {recomendados.map(item => <S.ListItem>{item}</S.ListItem>)}
+          </S.List>
+
+          <S.Title id="premium">Requisitos Premium</S.Title>
+
+          <S.List>
+          {premium.map(item => <S.ListItem>{item}</S.ListItem>)}
+          </S.List>
+
+          <S.Title id="artigo">Artigo completo</S.Title>
+
+          <S.Paragraph>Para ler o artigo completo acesse esse{' '}
+            <span><a href="https://www.buscape.com.br/console-de-video-game/conteudo/valorant#inicio" target="_blank" rel="noreferrer" >Link</a></span>. 
+            Autor do artigo <span>Filipe Salles</span>.</S.Paragraph>
+        </S.ContentTexts>
+      </S.Container>
+    </S.Wrapper>
   )
 }
 
